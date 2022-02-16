df66a82814ab68eef6c1d5dc19a475a4943ba2f9
AtharvSaraswat123 <87174532+AtharvSaraswat123@users.noreply.github.com>
2022-02-16 18:00:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+difference=0;
+rightWristX=0;
+leftWristX=0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,500);
+    canvas.position(560,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log('poseNet is Initialized!');
+}
+function draw(){
+    background(0,0,255);
+    textSize(difference);
+    fill('#000000');
+    text('Atharv',50,400)    
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    }
+}
